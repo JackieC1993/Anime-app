432955f6933f35b317ef472fa91fe4fe9a80ed14
JackieC1993 <jackiecheung@pursuit.org>
2024-01-23 12:57:55 -0500

    added anime to the frontend

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './Components/Header'
+import Search from './Components/Search'
+import Main from './Components/Main'
+import Footer from './Components/Footer'
+
+function App() {
+  const [anime, setAnime] = useState(null)
+
+  return (
+    <div className="app">
+    <Header />
+    <Search setAnime ={setAnime}/>
+    <Main anime={anime}/>
+    <Footer/>
+    </div>
+  )
+}
+
+export default App;
+ 
+
+ 
+
+export default App
---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import pokemonLogo from '../assets/pokemon-logo.png'
+
+const Header = () => {
+    return (
+        <header>
+            <img className="logo"
+            src={AnimeLogo} alt="Anime Logo"/>
+        </header>
+    );
+};
+
+export default Header;
